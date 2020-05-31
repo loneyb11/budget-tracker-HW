783ad3a944253579843d13d5709afcfed67cd2f3
Ben Loney <benloney@gmail.com>
2020-05-30 23:04:05 -0500

    update to files

--- Develop/public/db.js
+++ Develop/public/db.js
@@ -8,6 +8,6 @@ let db;
 const request = indexedDB.open("budget", 1);
 
 request.onupgradeneeded = ({ target }) => {
-    let db target.result;
+    let db = target.result;
     db.createObjectStore("pending", { autoIncrement: true });
-}
+};
---
+++ Develop/public/service-worker.js
@@ -0,0 +1,33 @@
+var CACHE_NAME = 
+
+var urlsToCache = [
+    '/',
+    '/styles.css',
+    'index.js'
+];
+
+
+self.addEventListener('install', function(event){
+    event.waitUntil(
+        canches.open(CACHE_NAME)
+        .then(function(cache){
+            console.log('Cache opened');
+            return cache.addAll(urlsToCache);
+        })
+    );
+    });
+
+    self.addEventListener("fetch", function(event){
+        event.respondWith(
+            fetch(event.request).catch(function(){
+                return caches.match(event.request).then(function(response){
+                    if (response) {
+                        return response;
+                    } else if (event.request.headers.get("accept").includes
+                    ("text/html")){
+                        return caches.match("/index.html");
+                    }
+                });
+            })
+        );
+        });
