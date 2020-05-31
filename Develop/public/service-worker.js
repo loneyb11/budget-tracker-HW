1f4619f3c9f9e1ce4d42d0acc67941ce2163b13d
Ben Loney <benloney@gmail.com>
2020-05-31 02:55:52 -0500

    npm edits

--- Develop/public/service-worker.js
+++ Develop/public/service-worker.js
@@ -1,33 +1,34 @@
 var CACHE_NAME = 'my-site-cache-v1';
 
 var urlsToCache = [
-    '/',
-    '/styles.css',
-    '/index.js'
+  '/',
+  '/styles.css',
+  'index.js'
 ];
 
 
-self.addEventListener('install', function(event){
-    event.waitUntil(
-        canches.open(CACHE_NAME)
-        .then(function(cache){
-            console.log('Cache opened');
-            return cache.addAll(urlsToCache);
-        })
-    );
-    });
+self.addEventListener('install', function(event) {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
 
-    self.addEventListener("fetch", function(event){
-        event.respondWith(
-            fetch(event.request).catch(function(){
-                return caches.match(event.request).then(function(response){
-                    if (response) {
-                        return response;
-                    } else if (event.request.headers.get("accept").includes
-                    ("text/html")){
-                        return caches.match("/index.html");
-                    }
-                });
-            })
-        );
-        });
+self.addEventListener("fetch", function(event) {
+  event.respondWith(
+    fetch(event.request).catch(function() {
+      return caches.match(event.request).then(function(response) {
+        if (response) {
+          return response;
+        } else if (event.request.headers.get("accept").includes("text/html")) {
+          return caches.match("/index.html");
+        }
+      });
+    })
+  );
+});
+
