00702ecef01604a91432d459839a866004e58e2a
Ben Loney <benloney@gmail.com>
2020-05-31 21:23:51 -0500

    image size change

--- server.js
+++ server.js
@@ -10,7 +10,9 @@ const app = express();
 app.use(logger("dev"));
 
 app.use(compression());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({
+  extended: true
+}));
 app.use(express.json());
 
 app.use(express.static("public"));
