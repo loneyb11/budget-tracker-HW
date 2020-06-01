a060009432fc901dd72ff173541960b3720026e5
Ben Loney <benloney@gmail.com>
2020-05-31 19:27:36 -0500

    mongo heroku

--- Develop/server.js
+++ Develop/server.js
@@ -15,7 +15,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://heroku_hb6cpb0j:2510bjee4382@ds031223.mlab.com:31223/heroku_hb6cpb0j", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://heroku_hb6cpb0j:38qr48k2n8idp2b9gvdvns40ik@ds031223.mlab.com:31223/heroku_hb6cpb0j", {
   useUnifiedTopology: true,
   useNewUrlParser: true,
   useFindAndModify: false
