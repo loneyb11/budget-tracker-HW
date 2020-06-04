65ded8fcff031d2dcab724092c459a4066fd9107
Ben Loney <benloney@gmail.com>
2020-06-04 15:30:42 -0500

    cleaning up code

--- public/service-worker.js
+++ public/service-worker.js
@@ -4,7 +4,10 @@ var urlsToCache = [
   '/',
   '/styles.css',
   '/index.js',
-  '/db.js'
+  '/index.html',
+  '/db.js',
+  '/icons/icon-192x192.png',
+  '/icons/icon-512x512.png'
 ];
 
 
--- server.js
+++ server.js
@@ -18,14 +18,14 @@ app.use(express.json());
 app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://heroku_hb6cpb0j:38qr48k2n8idp2b9gvdvns40ik@ds031223.mlab.com:31223/heroku_hb6cpb0j", {
-  useUnifiedTopology: true,
   useNewUrlParser: true,
-  useFindAndModify: false
+  useFindAndModify: false,
+  useUnifiedTopology: true
 });
 
 // routes
-app.use(require("./routes/api.js"))
+app.use(require("./routes/api.js"));
 
 app.listen(PORT, () => {
-  console.log(`App running on port ${PORT}!`)
-})
+  console.log(`App running on port ${PORT}!`);
+});
