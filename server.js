809c136429263bccaa3b670324b4347945794a16
Ben Loney <benloney@gmail.com>
2020-06-04 11:59:12 -0500

    express change

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(express.urlencoded({
 }));
 app.use(express.json());
 
-app.use(express.static("public"));
+
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://heroku_hb6cpb0j:38qr48k2n8idp2b9gvdvns40ik@ds031223.mlab.com:31223/heroku_hb6cpb0j", {
   useUnifiedTopology: true,
